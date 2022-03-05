44bdd0362231c01a5a1d07b7eb0fb55f07fe9648
sinknotswim <83678633+sinknotswim@users.noreply.github.com>
2022-03-04 20:38:59 -0600

    Add files via upload

---
+++ exercise7.py
@@ -0,0 +1,25 @@
+#1 4-3
+
+for value in range(1,21):
+    print(value)
+
+#2 4-6
+
+odd_numbers = range(1, 21, 2)
+
+for value in odd_numbers:
+    print(value)
+
+#3 4-7
+
+tri_numbers = range(1, 31, 3)
+
+for value in tri_numbers:
+    print(value)
+
+#4 4-8
+
+cube_numbers = range(1, 11, 2**3)
+
+for value in cube_numbers:
+    print(value)
